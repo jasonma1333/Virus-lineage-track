bc50033c5f6f99befa2357b67cccf1592bd9d350
Ma Tsz Hin <tszhma2-c@my.cityu.edu.hk>
2025-07-16 12:44:15 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "cov-web",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build && next export",
+    "start": "next start"
+  },
+  "dependencies": {
+    "@tanstack/react-query": "^5.37.0",
+    "next": "14.2.3",
+    "react": "18.3.1",
+    "react-dom": "18.3.1",
+    "recharts": "^2.7.3"
+  },
+  "devDependencies": {
+    "typescript": "^5.5.0"
+  }
+}
